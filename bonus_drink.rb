a67611dd59e0c38dd16f1c4813b7dd5865c03cac
toyama satoshi <toyamarinyon@gmail.com>
2016-01-15 02:22:45 +0900

    Add improve plan

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
+    #
+    # TODO
+    #   要件を満たすには以下のコードでOKだが
+    #   コードだけでは理解できないので、
+    #   数学的な証明を記載したうえで差し替えたい
+    #
+    #   amount + amount.pred.abs / 2
+    #
     drinkable_bottles = amount
     drank_bottles = 0
     while drank_bottles < drinkable_bottles do
