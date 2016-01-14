7e80c567ece59875a7c9f04b5d5168e9db0b8ff8
toyama satoshi <toyamarinyon@gmail.com>
2016-01-15 01:55:41 +0900

    Implements bonus drink

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    drinkable_bottles = amount
+    drank_bottles = 0
+    while drank_bottles < drinkable_bottles do
+      drank_bottles+=1
+      if drank_bottles % 3 == 0
+        drinkable_bottles+=1
+      end
+    end
+    drank_bottles
   end
 end
